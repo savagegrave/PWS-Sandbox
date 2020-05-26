8b2b73b019fe18730439eb7163ed1c226aa8aa0b
savagegrave25 <savagegrave25@hotmail.co.uk>
2020-05-26 23:19:06 +0100

    Add project files.

---
+++ PWS Sandbox/PWS Sandbox/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PWS_Sandbox
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // Declare the main page as a navigation page!!  This will save alot of problems later
+            // Same can be done with tabbed etc etc.
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PWS Sandbox/PWS Sandbox/ViewModels/GetSomeData.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using PWS_Sandbox;
+namespace PWS_Sandbox.ViewModels
+{
+    class PWS_Sandbox : INotifyPropertyChanged
+        
+    {
+        #region property changed
+        
+        /*
+         * Now this bit of code, I learnt from James Montemagno, a developer for Microsoft.
+         * The link can be found in the readme.txt files.
+         * 
+         * This region will allow the view to update each time values in the data bindings change.  Its pretty cool!
+         */
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+
+        // Declare Some Variables.
+
+        #region Variables
+
+        /// <summary>
+        /// Username = Variable to hold the user's name.
+        /// </summary>
+        private string username = "Steven Houldey";
+        /// <summary>
+        /// Emailaddress = User's Email Address.
+        /// </summary>
+        private string emailaddress = "Hi";
+        
+        /// <summary>
+        /// Set Busy Indicator
+        /// </summary>
+        private bool imbusy = false;
+
+        #endregion
+
+        #region Getters and Setters.
+        // Getters and Setters
+        // ( I know this can be done in one line, but its easier to visualise)
+
+        /// <summary>
+        /// Set the return and set values as required. 
+        /// </summary>
+        public string UserName
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+                OnPropertyChanged();
+            }
+        }
+            
+        public string EmailAddress
+        {
+            get
+            {
+                return emailaddress;
+            }
+
+            set
+            {
+                emailaddress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string MessageToUser
+        {
+            get
+            {
+                return $"Hello {username} How are you today? Your Email Is: {emailaddress}";
+            }
+       
+        }
+
+        public bool ImBusy
+        {
+            get { return imbusy; }
+            set { 
+                imbusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+    }
+}
